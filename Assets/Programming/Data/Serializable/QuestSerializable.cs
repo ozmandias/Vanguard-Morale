2abc079eb9ac48a7eaa0af23192042b9a613d570
Myo Si Thu <myosithu53545556@gmail.com>
2025-11-30 21:31:35 +0630

    add: Quest Data - Kingdom of Fortis Quests

--- Assets/Programming/Data/Serializable/QuestSerializable.cs
+++ Assets/Programming/Data/Serializable/QuestSerializable.cs
@@ -2,8 +2,12 @@ using UnityEngine;
 
 [System.Serializable] public class QuestSerializable {
     public string questTitle;
+    public QuestInfo []questInfo;
+}
+
+[System.Serializable] public class QuestInfo {
     public string questDescription;
     public QuestType questType;
     public RewardType rewardType;
-    public GameObject []questObjects;
+    public GameObject questObject;
 }
